02c247de4cb92d37cc1f292f09e8fc9a2ea02dd5
Ellias Matheus <ellias_matheus@hotmail.com>
2017-10-07 22:27:47 -0300

    closes #7

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -30,6 +30,7 @@ class Troco {
         count = 0;
         while (valor % 10 != 0) {
             count++;
+            valor -= 10;
         }
         papeisMoeda[2] = new PapelMoeda(10, count);
         count = 0;
